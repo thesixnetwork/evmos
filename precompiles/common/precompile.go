bacdfb125f32c6a285b30df57dbbb38fd0c48926
dDeeDev <dee.hamdee.084@gmail.com>
2025-06-07 21:46:39 +0700

    feat(precompile): add vm.ContractRef as validation

--- precompiles/common/precompile.go
+++ precompiles/common/precompile.go
@@ -20,6 +20,7 @@ import (
 const UnknownMethodCallGas uint64 = 3000
 
 type PrecompileExecutor interface {
+	vm.ContractRef
 	RequiredGas([]byte, *abi.Method) uint64
 	Execute(ctx sdk.Context, method *abi.Method, caller common.Address, callingContract common.Address, args []interface{}, value *big.Int, readOnly bool, evm *vm.EVM) ([]byte, error)
 }
